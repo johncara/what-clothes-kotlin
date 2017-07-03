f51b901da87cad3c4f009c72e37e6c1109190847
Caracode <caracode@Johns-MacBook-Pro.local>
2017-07-03 13:13:19 -0500

    Added teardown to unit test

--- app/src/test/java/com/caracode/whatclothes/main/MainPresenterTest.java
+++ app/src/test/java/com/caracode/whatclothes/main/MainPresenterTest.java
@@ -1,11 +1,13 @@
 package com.caracode.whatclothes.main;
 
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 
+import static junit.framework.Assert.assertTrue;
 import static org.mockito.Mockito.verify;
 
 @RunWith(MockitoJUnitRunner.class)
@@ -26,5 +28,12 @@ public class MainPresenterTest {
     public void testAttach() {
         presenter.attachView(view);
         verify(view).showText("Hello 30 Inch");
+    }
+
+    @After
+    public void after() {
+        presenter.detachView();
+        presenter.destroy();
+        assertTrue(presenter.isDestroyed());
     }
 }
