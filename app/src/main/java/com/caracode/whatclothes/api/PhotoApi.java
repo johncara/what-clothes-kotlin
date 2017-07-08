a51bc507cdc0aa137f5581d884a44f5336c392bc
John O'Halloran <caracode.ltd@gmail.com>
2017-07-08 14:55:32 -0500

    Extracted query parameters for easier editing in photo api.

--- app/src/main/java/com/caracode/whatclothes/api/PhotoApi.java
+++ app/src/main/java/com/caracode/whatclothes/api/PhotoApi.java
@@ -4,9 +4,12 @@ import com.caracode.whatclothes.api.response.PhotosResponse;
 
 import io.reactivex.Single;
 import retrofit2.http.GET;
+import retrofit2.http.Query;
 
 public interface PhotoApi {
 
-    @GET("services/rest/?method=flickr.photos.search&api_key=9134eb4a1896a0f30f1814811f02a760&tags=scenery%2C+beautiful&lat=51.50853&lon=-0.12574&format=json&nojsoncallback=1&auth_token=72157685845978866-b9f24eada6e9f506&api_sig=9b62394de04066c44d8a62641d612f38")
-    Single<PhotosResponse> getPhotos();
+    @GET("/services/rest/")
+    Single<PhotosResponse> getPhotos(@Query("method") String method, @Query("api_key") String apiKey, @Query("tags") String tags, @Query("lat") double lat,
+                                     @Query("lon") double lon, @Query("format") String format, @Query("nojsoncallback") int noJsonCallback, @Query("auth_token") String authToken,
+                                     @Query("api_sig") String apiSig);
 }
--- app/src/main/java/com/caracode/whatclothes/service/PhotoService.java
+++ app/src/main/java/com/caracode/whatclothes/service/PhotoService.java
@@ -9,6 +9,18 @@ import io.reactivex.Single;
 
 public class PhotoService {
 
+    private final String API_KEY = "7d960f5068e6de7eded7322168c942e1";
+    private final String AUTH_TOKEN = "72157683288550223-600076ef4b961b32";
+    private final String API_SIG = "ba94aaa84ae3527a150a814e302bd809";
+
+    private final String METHOD = "flickr.photos.search";
+    private final String TAGS = "scenery, beautiful";
+    private final double LAT = 51.50853;
+    private final double LON = -0.12574;
+    private final String FORMAT = "json";
+    private final int NO_JSON_CALLBACK = 1;
+
+
     private final PhotoApi photoApi;
 
     public PhotoService(@NonNull PhotoApi photoApi) {
@@ -16,6 +28,6 @@ public class PhotoService {
     }
 
     public Single<PhotosResponse> getPhotos() {
-        return photoApi.getPhotos();
+        return photoApi.getPhotos(METHOD, API_KEY, TAGS, LAT, LON, FORMAT, NO_JSON_CALLBACK, AUTH_TOKEN, API_SIG);
     }
 }
