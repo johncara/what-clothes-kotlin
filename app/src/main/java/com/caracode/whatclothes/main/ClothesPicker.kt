307508a1e97f38e04c0720e29df1ae5040406731
John O'Halloran <caracode.ltd@gmail.com>
2019-02-12 14:04:22 +0000

    Change the ClothesPicker defaults to account better for winter weather.

--- app/src/main/java/com/caracode/whatclothes/main/ClothesPicker.kt
+++ app/src/main/java/com/caracode/whatclothes/main/ClothesPicker.kt
@@ -10,21 +10,19 @@ internal enum class ClothesPicker(private val minTempAbove: Double?,
 
     // ---------------------  UPPER WEAR  ----------------------------------
     TShirtWeather(15.0, 20.0, null, null, R.drawable.tshirt),
-    JacketWeather(null, null, 12.0, 18.0, R.drawable.jacket),
-    CoatWeather(null, null, 8.0, 12.0, R.drawable.coat),
+    JacketWeather(5.0, null, null, 12.0, R.drawable.jacket),
+    CoatWeather(null, null, 5.0, null, R.drawable.coat),
 
     // ---------------------  LOWER WEAR  ----------------------------------
     ShortsWeather(17.0, 22.0, null, null, R.drawable.shorts);
 
     companion object {
         fun getClothesUpperLower(maxTemp: Double, minTemp: Double): Pair<Int, Int> {
-
-
             val upper = if (minTemp > TShirtWeather.minTempAbove!! && maxTemp > TShirtWeather.maxTempAbove!!) {
                 TShirtWeather.drawableRes
-            } else if (minTemp < JacketWeather.minTempBelow!! && maxTemp < JacketWeather.maxTempBelow!!) {
+            } else if (minTemp > JacketWeather.minTempAbove && maxTemp < JacketWeather.maxTempBelow!!) {
                 JacketWeather.drawableRes
-            } else if (minTemp < CoatWeather.minTempBelow && maxTemp < CoatWeather.maxTempBelow!!) {
+            } else if (minTemp < CoatWeather.minTempBelow!!) {
                 CoatWeather.drawableRes
             } else {
                 R.drawable.jumper
